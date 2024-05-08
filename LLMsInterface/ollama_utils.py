048585c4d6cb4d1af500c3253d75c67e18056519
Vaibhav Chaudhari <vaibhav.chaudhari@iem.fraunhofer.de>
2024-05-08 07:59:03 +0200

    Add ollama_utils to check ollama connection and model availabilty

---
+++ LLMsInterface/ollama_utils.py
@@ -0,0 +1,39 @@
+import ollama
+import logging
+        
+logging.basicConfig(level=logging.INFO)
+
+class Ollama:
+    def __init__(self, base_url):
+        self.client = ollama.Client(base_url)
+
+    def is_running(self):
+        try:
+            self.client._request('GET', '')
+            logging.info("Connection to Ollama Host is successful.")
+            return True
+        
+        except Exception as e:
+            logging.error(f"Failed to connect to Ollama Host: {e}")
+            return False
+
+    def model_available(self, model):
+        try:
+            self.client.show(model)
+            logging.info(f"Model {model} is available.")
+            return True
+        
+        except Exception as e:
+            if (str(e) == f"model '{model}' not found"):
+                logging.error(f"Model {model} not available.")
+                logging.info(f"Attempting to download {model}...")
+                self.client.pull(model)
+                logging.info(f"Model downloaded: {model}")
+            else:
+                logging.error(f"Failed to connect to Ollama Host: {e}")
+                return False
+
+if __name__ == '__main__':  
+    ol = Ollama("http://localhost:11434")
+    # ol.is_running()
+    ol.model_available('llama3')

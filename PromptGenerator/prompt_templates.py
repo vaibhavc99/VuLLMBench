64e4b646301ab7625d286cb3101d51716e540636
Vaibhav Chaudhari <vaibhav.chaudhari@iem.fraunhofer.de>
2024-05-15 08:41:07 +0200

    Add top25 cwe list + updates

--- PromptGenerator/prompt_templates.py
+++ PromptGenerator/prompt_templates.py
@@ -1,7 +1,10 @@
 # Simple Prompt
 SIMPLE_SYSTEM_PROMPT = """
 You are an assistant with expertise in identifying and explaining security vulnerabilities within code samples.
-When providing a response, please use the following format exclusively: 'Vulnerability: <True or False> | Vulnerability Type: <CWE_number>'.
+When providing a response, please use the following format exclusively: 
+'Vulnerability: <True or False> | Vulnerability Type: <CWE_number> | Vulnerability Name: <Name of CWE>'.
+
+If there are no vulnerabilities, enter "N/A" in the other fields. Do not include any additional information in the response.
 """
 
 SIMPLE_USER_PROMPT = """
@@ -13,7 +16,10 @@ Is there any vulnerability in the following code?
 VUL_SYSTEM_PROMPT = SIMPLE_SYSTEM_PROMPT
 
 VUL_USER_PROMPT = """
-Is there any {cwe_list} vulnerability in the following code?
+Please review the following code snippet to determine if it contains any vulnerabilities related to the specified Common Weakness Enumeration (CWE) categories.
+CWE Categories: {cwe_list}
+
+Code Snippet:
 {code}
 """
 
@@ -23,8 +29,10 @@ You are an assistant with deep expertise in identifying, analyzing, and explaini
 When identifying a vulnerability, provide a detailed explanation of the vulnerability's nature, why it is a vulnerability, and the possible consequences if exploited.
 Your response should include whether a vulnerability is present, the type of vulnerability based on the CWE classification, and an in-depth analysis of the vulnerability.
 Please use the following format for your response: 
-'Vulnerability: <True or False> | Vulnerability Type: <CWE_number> | 
+'Vulnerability: <True or False> | Vulnerability Type: <CWE_number> | Vulnerability Name: <Name of CWE> |
 Explanation: <detailed explanation of the vulnerability, including potential consequences and why it is considered a vulnerability>.'
+
+If there are no vulnerabilities, enter "N/A" in the other fields. Do not include any additional information in the response.
 """
 
 EXPL_USER_PROMPT = """
@@ -38,8 +46,10 @@ You are an assistant with deep expertise in identifying, analyzing, and proposin
 When identifying a vulnerability, provide a solution or code modification to mitigate the vulnerability.
 Your response should include whether a vulnerability is present, the type of vulnerability based on the CWE classification, and a detailed solution or code modification that addresses the vulnerability effectively.
 Please use the following format for your response: 
-'Vulnerability: <True or False> | Vulnerability Type: <CWE_number> | 
+'Vulnerability: <True or False> | Vulnerability Type: <CWE_number> | Vulnerability Name: <Name of CWE> |
 Solution: <detailed description of the solution or code modification, including how it mitigates the vulnerability>.'
+
+If there are no vulnerabilities, enter "N/A" in the other fields. Do not include any additional information in the response.
 """
 
 SOL_USER_PROMPT = """

197dbfb7ef6c5da8a1cd1c9e6e25513f0bf0d5f3
Vaibhav Chaudhary <vaibhavchaudhari676@gmail.com>
2024-07-10 09:04:59 +0200

    Add self-reflection prompt generator that uses chat history.

--- PromptGenerator/prompt_templates.py
+++ PromptGenerator/prompt_templates.py
@@ -69,3 +69,31 @@ SOL_USER_PROMPT = """
 Please analyze the following code for vulnerabilities and suggest modifications to enhance security: 
 {code}
 """
+
+#test
+TEST_SYSTEM_PROMPT = """
+You are an assistant with expertise in identifying and explaining security vulnerabilities within code samples.
+When analyzing the code, ensure to trace the flow of all user inputs and their influence on any executed statements, especially focusing on how they may affect security.
+
+When providing a response, please use the following format exclusively:
+'Vulnerability: <True or False> | Vulnerability Type: <CWE_number> | Vulnerability Name: <Name of CWE>'.
+
+If there are no vulnerabilities, enter "N/A" in the other fields. Do not include any additional information in the response.
+"""
+
+TEST_USER_PROMPT = """
+Is there any vulnerability in the following code? Ensure to trace the flow of user inputs and their influence on executed statements.
+{code}
+"""
+
+
+SELF_REFLECTION_PROMPT = "Are the predictions provided in the previous response correct?"
+# SELF_REFLECTION_PROMPT = "Are you sure code contains the vulnerability?"
+
+# SELF_REFLECTION_PROMPT = "Are you sure?"
+
+# SELF_REFLECTION_PROMPT = """
+# Are the predictions provided in the previous response correct? 
+# If so, please confirm by responding in the specified format. 
+# If not, please provide the correct response in the specified format.
+# """

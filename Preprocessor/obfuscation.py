421e4f8929ffc343422ca1aa06d3c457464d90de
Vaibhav Chaudhary <vaibhavchaudhari676@gmail.com>
2024-08-07 08:48:59 +0200

    Update renaming logic

--- Preprocessor/obfuscation.py
+++ Preprocessor/obfuscation.py
@@ -2,7 +2,7 @@ import javalang
 import re
 import hashlib
 
-def _rename(name, prefix):
+def rename(name, prefix):
     """
     Rename the given name by appending a prefix and a hash of the name.
 
@@ -48,19 +48,19 @@ def obfuscate(source_code, classes=True, methods=True, variables=True, parameter
             testcase_name = re.search(r'BenchmarkTest(\d{5})', node.name)
             if testcase_name:
                 testcase_number = testcase_name.group(1)
-                new_class_name = f'Class{testcase_number}'
+                new_class_name = f'TestClass{testcase_number}'
                 modifications[node.name] = new_class_name
             else:
-                modifications[node.name] = f'Class_{hashlib.md5(node.name.encode()).hexdigest()[:8]}'
+                modifications[node.name] = f'TestClass_{hashlib.md5(node.name.encode()).hexdigest()[:8]}'
 
         elif isinstance(node, javalang.tree.MethodDeclaration) and methods:
-            modifications[node.name] = _rename(node.name, 'method')
+            modifications[node.name] = rename(node.name, 'method')
             
         elif isinstance(node, javalang.tree.VariableDeclarator) and variables and node.name != 'serialVersionUID' and node.name != 'length':
-            modifications[node.name] = _rename(node.name, 'var')
+            modifications[node.name] = rename(node.name, 'var')
 
         elif isinstance(node, javalang.tree.FormalParameter) and parameters:
-            modifications[node.name] = _rename(node.name, 'param')
+            modifications[node.name] = rename(node.name, 'param')
 
     # Avoid replacing "value" in @WebServlet annotations
     webservlet_patterns = re.findall(r'@WebServlet\s*\(.*?value\s*=\s*\".*?\"\)', modified_code)
@@ -80,7 +80,7 @@ def obfuscate(source_code, classes=True, methods=True, variables=True, parameter
     return modified_code, new_class_name
 
 if __name__ == '__main__':
-    with open('./BenchmarkTest00292.java', 'r') as file:
+    with open('./BenchmarkTest00116.java', 'r') as file:
         java_code = file.read()
 
     modified_code, new_class_name = obfuscate(java_code, classes=True, methods=False, variables=True, parameters=True)

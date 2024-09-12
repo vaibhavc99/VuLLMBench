4a7c4e332a337d1b66ddde84ad80244b6ee8a4a9
Vaibhav Chaudhari <vaibhav.chaudhari@iem.fraunhofer.de>
2024-09-12 15:53:48 +0200

    Added support for Azure OpenAI models

--- LLMsInterface/llms.py
+++ LLMsInterface/llms.py
@@ -1,6 +1,7 @@
-from langchain_openai import ChatOpenAI
+from langchain_openai import ChatOpenAI, AzureChatOpenAI
 from langchain_community.chat_models import ChatOllama
 from langchain_groq import ChatGroq
+from langchain_community.callbacks import get_openai_callback
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from LLMsInterface.ollama_utils import OllamaUtils
 import time
@@ -18,6 +19,10 @@ class LLMs:
         self.config = config
         self.ollama_checked = set()
         self.logger = configure_logging("LLMsInterface")
+        self.total_cost = 0.0
+        self.total_tokens = 0
+        self.prompt_tokens = 0
+        self.completion_tokens = 0
         
     def get_model(self, model_name):
         """
@@ -36,6 +41,15 @@ class LLMs:
                 temperature=self.config.MODEL_PARAMETERS["temperature"],
                 max_tokens=self.config.MODEL_PARAMETERS["max_tokens"]
             )
+                    
+        elif model_name in self.config.AZURE_OPENAI_MODEL_LIST:
+            model = AzureChatOpenAI(
+                azure_endpoint=self.config.AZURE_OPENAI_ENDPOINT,
+                azure_deployment=model_name,
+                api_key=self.config.AZURE_OPENAI_API_KEY,
+                api_version=self.config.AZURE_OPENAI_API_VERSION,
+                temperature=self.config.MODEL_PARAMETERS["temperature"]
+            )
 
         elif model_name in self.config.OLLAMA_MODEL_LIST:
             if model_name not in self.ollama_checked:
@@ -45,7 +59,7 @@ class LLMs:
                     return None
             
             model = ChatOllama(
-                base_url=self.config.OLLAMA_HOST,
+                base_url=self.config.OLLAMA_HOST_URL,
                 model=model_name,
                 temperature=self.config.MODEL_PARAMETERS["temperature"],
                 num_predict=self.config.MODEL_PARAMETERS["max_tokens"],
@@ -61,8 +75,8 @@ class LLMs:
 
         elif model_name in self.config.HOC_MODEL_LIST:
             model = ChatOpenAI(
-                openai_api_key="EMPTY", 
-                openai_api_base="https://hoc-lx-gpu02.ad.iem-hoc.de:8080/v1",
+                api_key="EMPTY", 
+                base_url=self.config.HOC_HOST_URL,
                 model=model_name,
                 temperature=self.config.MODEL_PARAMETERS["temperature"],
                 max_tokens=self.config.MODEL_PARAMETERS["max_tokens"]
@@ -86,8 +100,17 @@ class LLMs:
         """
         while True:
             try:
-                response = model.invoke(prompt)
-                return response.content
+                if model.__class__.__name__ == "AzureChatOpenAI":
+                    with get_openai_callback() as cb:
+                        response = model.invoke(prompt)
+                        self.total_cost += cb.total_cost
+                        self.total_tokens += cb.total_tokens
+                        self.prompt_tokens += cb.prompt_tokens
+                        self.completion_tokens += cb.completion_tokens
+                        return response.content
+                else:
+                    response = model.invoke(prompt)
+                    return response.content
             except Exception as e:
                 error_message = str(e)
                 self.logger.error(f"Error invoking model: {error_message}")
@@ -140,26 +163,30 @@ class LLMs:
         responses = {}
         try:
             with ThreadPoolExecutor(max_workers=10) as executor:
-                futures = {}
                 for model_name, prompts in parallel_prompt_models.items():
-                    start_time = time.time()
                     model = self.get_model(model_name)
                     if model is None:
                         self.logger.error(f"Model {model_name} could not be retrieved.")
                         continue
-
+                    
+                    start_time = time.time()
+                    futures = {}
                     for index, prompt in prompts:
                         futures[(index, model_name)] = executor.submit(self.get_response, prompt, model)
 
-                for key, future in futures.items():
-                    try:
-                        responses[key] = future.result()
-                        self.logger.info(f"Got response from {key[1]} at index {key[0]}")
-                    except Exception as e:
-                        self.logger.error(f"Failed to get response for {key}: {e}")
+                    for key, future in futures.items():
+                        try:
+                            responses[key] = future.result()
+                            self.logger.info(f"Got response from {key[1]} at index {key[0]}")
+                        except Exception as e:
+                            self.logger.error(f"Failed to get response for {key}: {e}")
 
-                elapsed_time = time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time))
-                self.logger.info(f"Model {model_name} executed {len(prompts)} prompts in {elapsed_time}")
+                    elapsed_time = time.strftime("%H:%M:%S", time.gmtime(time.time() - start_time))
+                    if model_name in self.config.AZURE_OPENAI_MODEL_LIST:
+                        self.logger.info(f"Model {model_name} executed {len(prompts)} prompts in {elapsed_time}")
+                        self.logger.info(f"Total cost: {self.total_cost} USD, Total tokens: {self.total_tokens}, Prompt tokens: {self.prompt_tokens}, Completion tokens: {self.completion_tokens}")
+                    else:
+                        self.logger.info(f"Model {model_name} executed {len(prompts)} prompts in {elapsed_time}")
 
         except (KeyboardInterrupt, SystemExit):
             self.logger.warning("Program interrupted. Returning collected responses so far.")
@@ -201,10 +228,14 @@ class LLMs:
 if __name__ == '__main__':
     import Utils.model_config as model_config
     llms = LLMs(model_config)
+    prompts = [(0, "Translate the sentence to German: 'I love programming'."),
+              (1, "Translate the sentence to Spanish: 'I am a software engineer'."),
+              (2, "Translate the sentence to French: 'I am learning new languages'.")]
     prompts_per_model = {
-        "hugging-quants/Meta-Llama-3.1-70B-Instruct-AWQ-INT4": [(0, "Translate the sentence to German: 'I love programming'.")],
-        "llama3.1:8b-instruct-q4_0": [(0, "Translate the sentence to German: 'I love programming'."),
-                         (1, "Translate the sentence to Spanish: 'I am a software engineer'.")]
+        "hugging-quants/Meta-Llama-3.1-70B-Instruct-AWQ-INT4": prompts,
+        # "llama3.1:8b": prompts,
+        "gpt-35-turbo-16k-0613": prompts,
+        # "gpt-4o-2024-05-13": prompts
     }
 
     responses = llms.query_llms(prompts_per_model)
